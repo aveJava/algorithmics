3ce4f14bac7e82f651b20eff061adb6567696c93
Nikita Koshelev <39601787+HuKuTa94@users.noreply.github.com>
2020-12-23 23:46:01 +0500

    added comment line in Problem5 solution and simplified logic a little bit

--- src/main/java/projecteuler/net/Problem5.java
+++ src/main/java/projecteuler/net/Problem5.java
@@ -12,27 +12,31 @@ class Problem5
         int to = 20;
         int result = getSolution( 1, 20 );
 
-        System.out.format( "%d - самое маленькое число которое делится без остатка на все числа %d до %d", result, from, to );
+        System.out.format( "%d - самое маленькое число которое делится без остатка на все числа от %d до %d", result, from, to );
     }
 
     static int getSolution( int from, int to )
     {
+        // Начинаем поиск наименьшего кратного с максимального множителя to
         int number = to;
         while( true )
         {
+            // Перемножаем число на все множители от to до from
             for( int multiplier = to; multiplier >= from; multiplier-- )
             {
-                // Данное число не поделилось без остатка
-                if( number % multiplier != 0 ) {
+                // Данное число не поделилось без остатка, переходим к следующему
+                if( number % multiplier != 0 )
+                {
+                    number++;
                     break;
                 }
 
                 // Все итерации пройдены для данного числа, значит оно искомое наименьшее кратное
-                if( multiplier == from ) {
+                if( multiplier == from )
+                {
                     return number;
                 }
             }
-            number++;
         }
     }
 }
